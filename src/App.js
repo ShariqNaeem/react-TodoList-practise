f9cbc2327d48e81b47142404ecaa043b2fa1cd8c
ShariqNaeem <muhammadshariq.64@gmail.com>
2019-11-11 16:43:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {/* <div className="col-4 bg-warning">
+        hello
+        </div>
+        <div className="col-4 bg-danger">
+          shariq
+        </div>
+        <div className="col-4 bg-primary">
+          Naeem
+        </div> */}
+        <TodoInput/>
+        <TodoList/>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <h6>Hello from INPUTS.</h6>
+            </div>
+        )
+    }
+}
