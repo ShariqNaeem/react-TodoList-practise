fda48592998af26f1c9b557b5864b11fd33599f2
ShariqNaeem <muhammadshariq.64@gmail.com>
2019-11-11 18:12:08 +0500

    create basic functions

--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoItem from './components/TodoItem';
 
 function App() {
+  state={
+    items:[{id:1, title:"wake up"},{id:2, title:"make breakfast"}],
+    id: uuid(),
+    item: '',
+    editItem: ''
+  }
+
+  handleChange=e=>{
+    console.log("Handle Change.");
+  }
+
+  handleSubmit=e=>{
+    console.log("Handle Submit.");
+  }
+  
+  clearList=()=>{
+    console.log("Clear List.");
+  }
+
+  
+  handleDelete=(id)=>{
+    console.log("Handle Delete.");
+  }
+  
+  handleEdit=(id)=>{
+    console.log("Handle Edit.");
+  }
+
+
   return (
     <div className="container">
       <div className="row">
-        {/* <div className="col-4 bg-warning">
-        hello
-        </div>
-        <div className="col-4 bg-danger">
-          shariq
-        </div>
-        <div className="col-4 bg-primary">
-          Naeem
-        </div> */}
-        <TodoInput/>
-        <TodoList/>
+        <div className=""> </div>
       </div>
     </div>
   );
