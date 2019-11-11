9141f4550fbfe0c299f7b2a0d68c5028f31ff287
ShariqNaeem <muhammadshariq.64@gmail.com>
2019-11-11 19:08:34 +0500

    App component structure

--- src/App.js
+++ src/App.js
@@ -12,34 +12,48 @@ function App() {
     id: uuid(),
     item: '',
     editItem: ''
-  }
+  };
 
   handleChange=e=>{
     console.log("Handle Change.");
-  }
+  };
 
   handleSubmit=e=>{
     console.log("Handle Submit.");
-  }
+  };
   
   clearList=()=>{
     console.log("Clear List.");
-  }
+  };
 
   
   handleDelete=(id)=>{
     console.log("Handle Delete.");
-  }
+  };
   
   handleEdit=(id)=>{
     console.log("Handle Edit.");
-  }
+  };
 
 
   return (
     <div className="container">
       <div className="row">
-        <div className=""> </div>
+        <div className="col-10 mx-auto col-md-8 mt-5">
+          <h3>TODO input</h3>
+          <TodoInput
+            item={this.state.item}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            editItem={this.state.editItem}
+          />
+          <TodoList
+            items={this.state.items}
+            clearList={this.clearList}
+            handleDelete={this.handleDelete}
+            handleEdit={this.handleEdit}
+          />
+        </div>
       </div>
     </div>
   );
